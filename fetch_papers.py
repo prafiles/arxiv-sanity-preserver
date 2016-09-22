985c1519ffc963aa28982ddd974851ffa330ac9c
Prakhar Shukla <prafiles@yahoo.com>
2016-09-22 10:09:10 +0530

    update query

--- fetch_papers.py
+++ fetch_papers.py
@@ -48,7 +48,7 @@ if __name__ == "__main__":
   parser = argparse.ArgumentParser()
   parser.add_argument('--db_path', dest='db_path', type=str, default='db.p', help='database pickle filename that we enrich')
   parser.add_argument('--search_query', dest='search_query', type=str,
-                      default='cat:cs.CV+OR+cat:cs.AI+OR+cat:cs.LG+OR+cat:cs.CL+OR+cat:cs.NE+OR+cat:stat.ML',
+                      default='cat:physics.acc-ph+OR+cat:physics.atom-ph+OR+cat:physics.class-ph+OR+cat:physics.comp-ph+OR+cat:physics.data-an+OR+cat:physics.gen-ph+OR+cat:physics.pop-ph+OR+cat:physics.space-ph',
                       help='query used for arxiv API. See http://arxiv.org/help/api/user-manual#detailed_examples')
   parser.add_argument('--start_index', dest='start_index', type=int, default=0, help='0 = most recent API result')
   parser.add_argument('--max_index', dest='max_index', type=int, default=10000, help='upper bound on paper index we will fetch')

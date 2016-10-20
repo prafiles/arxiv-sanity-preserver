e673ec95ca1f251a48e8b10dfb13af945ee963fb
Prakhar Shukla <prafiles@yahoo.com>
2016-10-20 19:33:37 +0530

    revert to prev method and reduce nof features

--- analyze.py
+++ analyze.py
@@ -18,27 +18,21 @@ db = pickle.load(open('db.p', 'rb'))
 
 # read all text files for all papers into memory
 pids = []
-n = 0
 
 def make_corpus():
-  global n
+  n = 0
   for pid, j in db.iteritems():
     n += 1
     idvv = '%sv%d' % (j['_rawid'], j['_version'])
     fname = os.path.join('txt', idvv) + '.pdf.txt'
     if os.path.isfile(fname):  # some pdfs dont translate to txt
-      yield process_file(fname, idvv)
-
-def process_file(fname,idvv):
-  global n
-  txt = open(fname, 'r').read()
-  if len(txt) > 100:  # way too short and suspicious
-    pids.append(idvv)
-    print 'read %d/%d (%s) with %d chars' % (n, len(db), idvv, len(txt))
-    return txt  # todo later: maybe filter or something some of them
-  else:
-    print 'skipped %d/%d (%s) with %d chars: suspicious!' % (n, len(db), idvv, len(txt))
-
+      txt = open(fname, 'r').read()
+      if len(txt) > 100:  # way too short and suspicious
+        pids.append(idvv)
+        print 'read %d/%d (%s) with %d chars' % (n, len(db), idvv, len(txt))
+        yield txt  # todo later: maybe filter or something some of them
+      else:
+        print 'skipped %d/%d (%s) with %d chars: suspicious!' % (n, len(db), idvv, len(txt))
 
   # compute tfidf vectors with scikits
 v = TfidfVectorizer(input='content',
@@ -48,7 +42,7 @@ v = TfidfVectorizer(input='content',
         ngram_range=(1, 2), max_features = 10000,
         norm='l2', use_idf=True, smooth_idf=True, sublinear_tf=False)
 
-X = v.fit_transform(make_corpus()) # TODO: custom function here
+X = v.fit_transform(make_corpus())
 print v.vocabulary_
 print X.shape
 
